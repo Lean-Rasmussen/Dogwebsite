48bb5f83ad487bec007360b25d3bab5240215fdd
Lean Rasmussen <leanmkrasmussen@gmail.com>
2018-01-24 14:32:13 +0100

    added submition section, with side scroll to email form

---
+++ src/omOs.js
@@ -0,0 +1,15 @@
+var OmosViewText1 = function(){
+	$(".omos_text1").css("transform", "translateX(0%)");
+	$(".omos_text2").css("transform", "translateX(150%)");
+	$(".omos_text3").css("transform", "translateX(300%)");
+};
+var OmosViewText2 = function(){
+	$(".omos_text1").css("transform", "translateX(-150%)");
+	$(".omos_text2").css("transform", "translateX(0)");
+	$(".omos_text3").css("transform", "translateX(150%)");
+};
+var OmosViewText3 = function(){
+	$(".omos_text1").css("transform", "translateX(-300%)");
+	$(".omos_text2").css("transform", "translateX(-150%)");
+	$(".omos_text3").css("transform", "translateX(0%)");
+};
---
+++ src/tilmeld.js
@@ -0,0 +1,8 @@
+var tilmeldStartSide = function(){
+	$(".tilmeld__container__tilmeld-info").css("transform", "translateX(0%)");
+	$(".tilmeld__container__tilmeld-form").css("transform", "translateX(150%)");
+};
+var tilmeldForm = function(){
+	$(".tilmeld__container__tilmeld-info").css("transform", "translateX(-150%)");
+	$(".tilmeld__container__tilmeld-form").css("transform", "translateX(0%)");
+};
