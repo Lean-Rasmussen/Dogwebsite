705c8cf0cf250c7ad6e7fc7f30fe7a115bb929c4
Lean Rasmussen <leanmkrasmussen@gmail.com>
2018-01-28 20:48:45 +0100

    added design for 750px screen width on some of site

--- src/tilmeld.js
+++ src/tilmeld.js
@@ -1,8 +1,12 @@
 var tilmeldStartSide = function(){
+	if(window.innerWidth < 750){
 	$(".tilmeld__container__tilmeld-info").css("transform", "translateX(0%)");
 	$(".tilmeld__container__tilmeld-form").css("transform", "translateX(150%)");
+	}
 };
 var tilmeldForm = function(){
+	if(window.innerWidth < 750){
 	$(".tilmeld__container__tilmeld-info").css("transform", "translateX(-150%)");
 	$(".tilmeld__container__tilmeld-form").css("transform", "translateX(0%)");
+	}
 };
